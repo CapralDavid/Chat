dd928b3030a0731b2619d3b4f57bc35f5350273a
vladyslav-proga <54845344+vladyslav-proga@users.noreply.github.com>
2021-06-22 03:35:13 +0300

    Add user add controller

--- chat/server/users.js
+++ chat/server/users.js
@@ -1,29 +1,19 @@
-const users = [];
+'use strict';
 
-const addUser = ({ id, name, room }) => {
-  name = name.trim().toLowerCase();
-  room = room.trim().toLowerCase();
+const Router = require('koa-router');
 
-  const existingUser = users.find((user) => user.room === room && user.name === name);
+const User = require('../models/User');
 
-  if(!name || !room) return { error: 'Username and room are required.' };
-  if(existingUser) return { error: 'Username is taken.' };
+const router = new Router().prefix('/users');
 
-  const user = { id, name, room };
+router.get('/:_id', async ctx => {
+  const { _id } = ctx.params;
+  const user = await User.findById(_id);
+  if (user) {
+    ctx.body = user;
+  } else {
+    ctx.throw(404);
+  }
+});
 
-  users.push(user);
-
-  return { user };
-}
-
-const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id);
-
-  if(index !== -1) return users.splice(index, 1)[0];
-}
-
-const getUser = (id) => users.find((user) => user.id === id);
-
-const getUsersInRoom = (room) => users.filter((user) => user.room === room);
-
-module.exports = { addUser, removeUser, getUser, getUsersInRoom };
+module.exports = router.routes();
